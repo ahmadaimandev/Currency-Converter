3d095dba911cc44d5d69c0c1c13a3b72c259eaff
Ahmad-Aiman <aimanzulbahari@gmail.com>
2023-04-25 11:49:22 +0800

    need to fix
    the javascript file not show

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+ const dropList = document.querySelectorAll(".drop-list select");
+
+ for (let i = 0; i< dropList.length; index++) {
+    for(currency_code in country_list ) {
+        console.log(currency_code);
+    }
+    
+ }
