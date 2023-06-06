a90d43b9ea3899282064b25e33c01f0c4dd0fc46
Ahmad Aiman <aimanzulbahari@gmail.com>
2023-06-06 11:41:57 +0800

    create gitignore

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
+ const Dotenv = require('dotenv-webpack');
+
+ module.exports = {
+    //other configuration options//
+    Plugins: [
+        new Dotenv()
+    ]
+ };
+ const apiKey = process.env.apiKey;
+ 
  const dropList = document.querySelectorAll(".drop-list select");
 
  for (let i = 0; i< dropList.length; index++) {
