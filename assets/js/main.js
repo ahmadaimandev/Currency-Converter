58634bea0d619ec674f4c1c3eb26b0727ed6adce
Ahmad Aiman <aimanzulbahari@gmail.com>
2023-07-06 08:01:14 +0800

    Update the html
    update the code by adding from and to select box for country list

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const dropList = document.querySelectorAll("form select"),
+fromCurrency = document.querySelector(".from select"),
+toCurrency = document.querySelector(".to select"),
+getButton = document.querySelector("form button");
+
+for (let i = 0; i < dropList.length; i++) {
+    for (let currency_code in country_list) {
+        q
+    }
+}
