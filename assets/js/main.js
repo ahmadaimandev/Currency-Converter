731670541766f3b6bb403e542118eeea3b737c55
Ahmad Aiman <aimanzulbahari@gmail.com>
2023-07-18 11:39:59 +0800

    add javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -159,3 +159,14 @@ let country_list = {
     "ZMK" : "ZM",
     "ZWD" : "ZW"
 }
+
+const dropList = document.querySelectorAll("form select"),
+fromCurrency = document.querySelector(".from select"),
+toCurrency = document.querySelector(".to select"),
+getButton = document.querySelector("form button");
+
+for (let i = 0; i < dropList.length; i++) {
+    for (let currency_code in country_list) {
+        
+    }
+}
